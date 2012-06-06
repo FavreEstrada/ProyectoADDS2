9f946557910d69cc6fda20dcbd7c57834c988db6
Solid Snake <viktor_zavala@unitec.edu>
2012-06-05 19:50:47 -0700

    Funciona agregar, modificar usuarios, modifica password y todo el bussiness

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -25,7 +25,9 @@ class UserController < ApplicationController
     
   def show
   end
-
+  def edit
+    @user = User.find(params[:id])
+  end
   def create
     @user = User.new(params[:user])
 
@@ -46,6 +48,19 @@ class UserController < ApplicationController
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @user }
+    end
+  end
+    
+  def update
+    @user = User.find(params[:id])
+    respond_to do |format|
+       if @user.update_attributes(params[:user])
+         format.html { redirect_to @user, notice: 'Barrio was successfully updated.' }
+         format.json { head :no_content }
+       else
+         format.html { render action: "edit" }
+         format.json { render json: @user.errors, status: :unprocessable_entity }
+       end
     end
   end
 
