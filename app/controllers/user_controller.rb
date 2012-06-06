4fd90d8352c4727df3b2073fafbf1f9f51daae8b
Solid Snake <viktor_zavala@unitec.edu>
2012-06-05 22:24:10 -0700

    Seguridad en layout completa y funcionando, agregados permisos de usuarios e implementados a perfil Administrador

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -24,7 +24,14 @@ class UserController < ApplicationController
   end
     
   def show
+      @user = User.find(params[:id])
+
+      respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @user }
+    end
   end
+
   def edit
     @user = User.find(params[:id])
   end
